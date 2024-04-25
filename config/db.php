d1e1eec3b0cfcea94765c40a545b6b0246d08d72
Monirul Islam <monirul.islam.sujon@gmail.com>
2024-04-26 01:48:56 +0600

    Working on the management system

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "user_management_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) 
+{
+  return "Connection failed: " . $conn->connect_error;
+}
---
+++ users.php
@@ -0,0 +1,46 @@
+<?php require_once __DIR__."/partials/head.php";?>
+<?php require_once __DIR__."/config/auth.php";?>
+
+    <body id="page-top">
+
+        <!-- Page Wrapper -->
+        <div id="wrapper">
+
+            <?php require_once __DIR__."/partials/sidebar.php";?>
+
+            <!-- Content Wrapper -->
+            <div id="content-wrapper" class="d-flex flex-column">
+
+                <!-- Main Content -->
+                <div id="content">
+
+                    <?php require_once __DIR__."/partials/nav.php";?>
+
+                    <!-- Begin Page Content -->
+                    <div class="container-fluid">
+
+                        <!-- Page Heading -->
+                        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                            <h2 class="h3 mb-0 text-gray-800">Users</h2>
+                        </div>
+
+
+
+                    </div>
+                    <!-- /.container-fluid -->
+
+                </div>
+                <!-- End of Main Content -->
+
+                <?php require_once __DIR__."/partials/footer.php";?>
+
+            </div>
+            <!-- End of Content Wrapper -->
+
+        </div>
+        <!-- End of Page Wrapper -->
+
+        
+        <?php require_once __DIR__."/partials/head.php";?>
+    </body>
+</html>
